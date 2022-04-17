f66c00ad0fcbda640483cdd95716c24100aaefed
djankeee13 <djani-b@live.com>
2022-04-17 21:48:21 +0200

    react project
    mozda ovaj put radio hmm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Best from './components/Best';
+import Featured from './components/Featured';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/navbar/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Best />
+      <Featured />
+      <Footer />
     </div>
   );
 }
